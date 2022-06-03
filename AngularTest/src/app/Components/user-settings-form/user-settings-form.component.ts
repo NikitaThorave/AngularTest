8f8d16f8bba6b0855956501e1f39f57ebde8dc42
NikitaThorave <nikita.thorave11@gmail.com>
2022-06-03 18:53:31 +0530

    form changes

--- AngularTest/src/app/Components/user-settings-form/user-settings-form.component.ts
+++ AngularTest/src/app/Components/user-settings-form/user-settings-form.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface UserSettings {
+  name: string;
+  emailOffers: boolean;
+  interfaceStyle: string;
+  subscriptionType: string;
+  notes: string;
+}
+
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  userSettings: UserSettings= {
+    name: 'Vivek Kumar',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'I like Angular framework',
+  }
 
   constructor() { }
 
